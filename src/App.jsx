b0c135c11a06bb7a1feb559136b942a08543e7de
Bekbolot <justworking8888@gmail.com>
2025-12-10 13:13:06 +0600

    [main] sdfdsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 function App() {
-  const [state, setState] = useState(0)
-  const [count, setCount] = useState(0)
-
-
-  useEffect(() => {
-    console.log('component did mount');
-
-  }, [state])
-  console.log(state);
-  function OLOLOLOLOLOO() {
-    setState(state + 1)
-  }
 
   return (
     <>
-      {state}
-      <button onClick={OLOLOLOLOLOO}>onClik</button>
+
+
+
+
+
     </>
   )
 }
